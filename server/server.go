bf470bd6414234aa273668050e95563ffa83bf27
Vaishakh GK <vaishakhgk2006@gmail.com>
2024-07-22 20:32:35 +0530

    feat: Websocket connection added

--- server/server.go
+++ server/server.go
@@ -1,12 +1,23 @@
 package server
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/contrib/websocket"
+)
 
 func Run(port string) {
 	var app = fiber.New()
 
 	app.Get("/test", func (c *fiber.Ctx) error {
 		return c.SendString("test route")
+	})
+
+	app.Use("/ws", func (c *fiber.Ctx) error {
+		if websocket.IsWebSocketUpgrade(c) {
+			return c.Next()
+		}
+
+		return fiber.ErrUpgradeRequired
 	})
 
 	app.Listen(":" + port)
